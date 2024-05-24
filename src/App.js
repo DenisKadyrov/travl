413a21f439ebba79849b4e76a2a7dbcfb6febb15
Denis <kripmrx@gmail.com>
2024-05-24 15:28:51 +0200

    create home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path='/' exact Component={Home} />
         </Routes>
       </Router>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Navbar.css'
@@ -18,16 +18,25 @@ function Navbar() {
       }
    }
 
+   useEffect(() => {
+      showButton();
+   }, []);
    window.addEventListener('resize', showButton)
 
    return (
    <>
       <nav className="navbar">
          <div className="navbar-container">
-            <Link to='/' className='navbar-logo'>
-               TRAVL <i className="fab fa-typo3"></i>
+            <Link 
+               to='/'
+               className='navbar-logo' 
+               onClick={clouseMobileMenu}
+            >TRAVL <i className="fab fa-typo3"></i>
             </Link>
-            <div className='menu-icon' onClick={handleMenuClick}>
+            <div
+               className='menu-icon'
+               onClick={handleMenuClick}
+            >
                <i className={click ? 'fas fa-times' : 'fas fa-bars'} ></i>
             </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
